858e98d66a48ed5424c9e98dedbe35f6d0afa905
Joann Mistica <joann@joannmistica.com>
2019-08-27 16:50:51 +0800

    Simplied version without the methods

--- biz_scraper.py
+++ biz_scraper.py
@@ -2,45 +2,32 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_numpages(soup):
-    numpages = soup.find("a", {"data-num-pages": True})
-    return int(numpages["data-num-pages"])
-
-def get_links(soup):
-    articles_list = list()
-    articles = soup.find_all("article", {"class": "not_mod"})
-    for article in articles:
-        articles_list.append(article.a["href"])
-    return articles_list
-
-def get_stories(link):
-    full_story = ""
-    response = requests.get(link)
-    soup = BeautifulSoup(response.text, "lxml")
-    subject = soup.find("h1", {"class": "entry"}).text
-    story = soup.find("div", {"class": "typography"})
-    blocks = story.find_all("p")
-    
-    for block in blocks:
-        full_story = full_story + block.text + "\n"
-    return dict({subject: full_story})
-
-
+# Retrieve all pages
 url = "https://www.bizarrepedia.com/crime/"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "lxml")
-pages = get_numpages(soup)
-links = list()
+pages = int(soup.find("a", {"data-num-pages": True})["data-num-pages"])
 
 for page in range(1, pages + 1):
-    # First page has no page number for Bizarrepedia
-    if page == 1:
+    if page == 1: # First page has no page number
         url = "https://www.bizarrepedia.com/crime"
     else:
         url = "https://www.bizarrepedia.com/crime/page/" + str(page)
+ 
+    # Retrieve each article
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "lxml")
-    links.extend(get_links(soup))
+    articles = soup.find_all("article", {"class": "not_mod"})
 
-for link in links:
-    print(get_stories(link))
+    for article in articles:
+        # Retrieve each story
+        response = requests.get(article.a["href"])
+        soup = BeautifulSoup(response.text, "lxml")
+        subject = soup.find("h1", {"class": "entry"}).text
+        story = soup.find("div", {"class": "typography"})
+        blocks = story.find_all("p")
+
+        for block in blocks:
+            print(block.text + "\n")
+        break
+    break
