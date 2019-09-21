72a74e1fda3aa86ef51a4dfd9fdd7fb400397eff
Joann Mistica <joann@joannmistica.com>
2019-09-21 23:46:27 +0800

    Modified for hubpages article

--- criminal_minds.py
+++ criminal_minds.py
@@ -2,22 +2,24 @@ import requests
 from bs4 import BeautifulSoup
 
 
-# Retrieve all profiles
+# Retrieve all stories
 url = "https://criminalminds.fandom.com/wiki/Real_Criminals/Serial_Killers"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "lxml")
-profiles = soup.find_all("div", {"class": "lightbox-caption"})
+stories = soup.find_all("div", {"class": "lightbox-caption"})
 
-for profile in profiles:
-    print(profile.find("a")["title"] + "\n")
+for story in stories:
     # Retrieve each story
-    url = "https://criminalminds.fandom.com" + profile.find("a")["href"]
+    url = "https://criminalminds.fandom.com" + story.find("a")["href"]
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "lxml")
-    desc = soup.find("div", {"id":"mw-content-text"})
-    print("Quote:\n" + desc.find("table").text.strip() + "\n")
-    ps = desc.find_all("p")
+    main_story = soup.find("div", {"id":"mw-content-text"})
+    quote = " ".join(main_story.find("table").text.split())
+    subject = story.find("a")["title"]
+    blocks = main_story.find_all("p")
+    full_story = ""
 
-    for p in ps:
-        print(p.text)
+    for block in blocks:
+        full_story = full_story + block.text + "\n"
+    print(quote + "\n" + subject + "\n\n" + full_story)
     break
