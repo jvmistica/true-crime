6459bb101ec89c6884dbca4f26438a753952a2a7
Joann Mistica <joann@joannmistica.com>
2019-09-27 16:59:12 +0800

    Added script to query records from Elasticsearch

--- elastic.py
+++ elastic.py
@@ -2,11 +2,22 @@ from elasticsearch import Elasticsearch
 es = Elasticsearch()
 
 
-def es_insert(category, source, subject, story):
+def es_insert(category, source, subject, story, **kwargs):
     doc = {
         "source": source,
         "subject": subject,
         "story": story,
+        **kwargs,
     }
     res = es.index(index=category, doc_type="story", body=doc)
     print(res["result"])
+
+def es_query():
+    res = es.search(index="truecrime", body={"query": {"match_all": {}}})
+    print("Got Hits:", res['hits']['hits'])
+    for hit in res['hits']['hits']:
+        print(hit["_source"]["source"])
+        print(hit["_source"]["subject"])
+        print(hit["_source"]["story"])
+
+es_query()
