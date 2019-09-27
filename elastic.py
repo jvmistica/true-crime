c724c7d3b49bbd01311070f1c36a999866de6976
Joann Mistica <joann@joannmistica.com>
2019-09-28 02:35:50 +0800

    Added query with search parameters.

--- elastic.py
+++ elastic.py
@@ -1,3 +1,4 @@
+import json
 from elasticsearch import Elasticsearch
 es = Elasticsearch()
 
@@ -12,12 +13,24 @@ def es_insert(category, source, subject, story, **kwargs):
     res = es.index(index=category, doc_type="story", body=doc)
     print(res["result"])
 
-def es_query():
-    res = es.search(index="truecrime", body={"query": {"match_all": {}}})
-    print("Got Hits:", res['hits']['hits'])
-    for hit in res['hits']['hits']:
-        print(hit["_source"]["source"])
-        print(hit["_source"]["subject"])
-        print(hit["_source"]["story"])
+def es_search(**kwargs):
+    result = dict()
+    result_set = list()
+    search_terms = list()
+    for key, value in kwargs.items():
+        search_terms.append({"match": {key: value}})
+ 
+    print(search_terms)
+    size = es.count(index="truecrime").get("count")
+    res = es.search(index="truecrime", size=size, body=json.dumps({"query": {"bool": {"must": search_terms}}}))
+    for hit in res["hits"]["hits"]:
+        result.update({"total": res["hits"]["total"], \
+                        "source": hit["_source"]["source"], \
+                        "subject": hit["_source"]["subject"], \
+                        "story": hit["_source"]["story"]})
+        if "quote" in hit["_source"]:
+            result.update({"quote": hit["_source"]["quote"]})
+        result_set.append(result)
+    return result_set
 
-es_query()
+#print(es_search(story="name"))
