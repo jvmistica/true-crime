ebf2ed42b6071f36321c49fa5dc29cb2b5c0d772
Joann Mistica <joann@joannmistica.com>
2019-10-13 00:46:49 +0800

    Added update method.

--- elastic.py
+++ elastic.py
@@ -13,18 +13,26 @@ def es_insert(category, source, subject, story, **extras):
     res = es.index(index=category, doc_type="story", body=doc)
     print(res["result"])
 
-def es_search(**filters):
+
+def es_update(category, id, **extras):
+    body = {"body": {"doc" : { **extras, } } }
+    res = es.update(index=category, doc_type="story", id=id, body=body)
+    print(res["result"])
+
+
+def es_search(**filters): #added id
     result = dict()
     result_set = list()
     search_terms = list()
     for key, value in filters.items():
         search_terms.append({"match": {key: value}})
  
-    print("Search terms: ", search_terms)
+    print("Search terms:", search_terms)
     size = es.count(index="truecrime").get("count")
     res = es.search(index="truecrime", size=size, body=json.dumps({"query": {"bool": {"must": search_terms}}}))
     for hit in res["hits"]["hits"]:
         result = {"total": res["hits"]["total"], \
+                        "id": hit["_id"], \
                         "source": hit["_source"]["source"], \
                         "subject": hit["_source"]["subject"], \
                         "story": hit["_source"]["story"]}
