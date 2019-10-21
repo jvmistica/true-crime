0a4fce5b6b3c583a7c2a1dd0c9c63b44b486afc3
Joann Mistica <joann@joannmistica.com>
2019-10-21 14:34:05 +0800

    Added sample usage scripts.

---
+++ extract_dates.py
@@ -0,0 +1,20 @@
+import re
+from modules.elastic import es_search, es_update
+from modules.extract import get_arrest_date
+
+
+for val in es_search():
+    arrests = list()
+    for result in re.finditer(r'(\w+\W+){0}(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)(\w+\W+)\d{1,4},?\s\d{0,4}(\w+\W+){1,10}(captured|caught|seized|arrested|apprehended)', val.get("story"), flags=re.I):
+        words = result.group().replace(",", "").split()
+        arrest_date = words[:(3 if words[2].isdigit() == True else 2)]
+        arrests.append(get_arrest_date(arrest_date))
+ 
+    for result in re.finditer(r'(\w+\W+){0}(captured|caught|seized|arrested|apprehended)\s(\w+\W+){1,10}(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)(\w+\W+)\d{1,4},?\s\d{0,4}', val.get("story"), flags=re.I):
+        words = result.group().replace(",", "").split()
+        arrest_date = words[(-3 if words[-2].isdigit() == True else -2):]
+        arrests.append(get_arrest_date(arrest_date))
+
+    if len(arrests) > 0:
+        print(val.get("subject"), arrests)
+        es_update("truecrime", val.get("id"), arrests=arrests)
